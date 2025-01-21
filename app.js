43a9eb5f7e448ed9ce2bccde593b7444b23ca0a1
Giorgia <106474194+giorgia834@users.noreply.github.com>
2025-01-21 09:56:46 +0000

    Add cors to project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express() //always use as express convention
 
 const logger = require("./logger")
 
 //Middleware
+app.use(cors())
 app.use(express.json())
 app.use(logger) //to run on any request
 
